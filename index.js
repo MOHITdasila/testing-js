1412ea3b0597e598828ea8095d1420c1e1e9f40c
MOHITdasila <dasila.m24@gmail.com>
2023-02-24 14:54:50 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-// Import stylesheets
-// import './style.css';
 
-// Write Javascript code!
-// const appDiv = document.getElementById('app');
-// appDiv.innerHTML = `<h1>JS Starter</h1>`;
 import {a} from "./helper.js"
 import add from "./helper.js"
 console.log(add(4,5))
-console.log('jhello',a);
+console.log('hello',a);
